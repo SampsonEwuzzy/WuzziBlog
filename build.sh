c9e2e210862c64e5c276e782f593c5b7ce3ad138
Sampson Ikechukwu Ewuzie <sampson.ewuzie@abujaelectricity.com>
2025-09-11 00:57:33 +0100

    Updated render.yaml

--- build.sh
+++ build.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit  
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --noinput
 
-# Create a superuser non-interactively if environment variables are set
 if [[ $DJANGO_SUPERUSER_USERNAME && $DJANGO_SUPERUSER_PASSWORD ]]; then
   python manage.py createsuperuser --no-input
 fi
 
-# Apply database migrations
 python manage.py migrate
+
