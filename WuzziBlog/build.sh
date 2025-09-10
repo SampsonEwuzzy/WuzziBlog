2a53a743a663055fe3224c48fcba04e1ae101183
SAMPSON EWUZIE <56512076+SampsonEwuzzy@users.noreply.github.com>
2025-09-10 15:34:14 +0100

    Update build.sh

--- WuzziBlog/build.sh
+++ WuzziBlog/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-set -o errexit
+# exit on error
+set -o errexit  
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Apply database migrations
 python manage.py migrate
