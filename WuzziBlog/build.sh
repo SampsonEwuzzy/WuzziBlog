8a11883d50d522ce87c47c886582c02240af77d0
SAMPSON EWUZIE <56512076+SampsonEwuzzy@users.noreply.github.com>
2025-09-10 20:47:01 +0100

    Update build.sh

--- WuzziBlog/build.sh
+++ WuzziBlog/build.sh
@@ -8,5 +8,13 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --noinput
 
+
+# Create a superuser non-interactively if environment variables are set
+if [[ $DJANGO_SUPERUSER_USERNAME && $DJANGO_SUPERUSER_PASSWORD ]]; then
+  python manage.py createsuperuser --no-input
+fi
+
+# Run migrations
+python manage.py migrate
 # Apply database migrations
 python manage.py migrate
